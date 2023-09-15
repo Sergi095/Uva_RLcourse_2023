cffd828474dd2c027843e0c2a9f6cea0c8fb58a9
Sergi095 <sergio.gutierrezmaury@gmail.com>
2023-09-15 21:43:51 +0200

    adding the second part of the lab

--- lab2/mc_autograde.py
+++ lab2/mc_autograde.py
@@ -126,7 +126,7 @@ def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_funct
     
     # YOUR CODE HERE
     
-    for i in range(num_episodes):
+    for i in tqdm(range(num_episodes)):
 
         states, actions, rewards, dones = sampling_function(env, policy)
 
@@ -161,7 +161,11 @@ class RandomBlackjackPolicy(object):
             Numpy array filled with probabilities (same length as states and actions)
         """
         # YOUR CODE HERE
-        raise NotImplementedError
+        # raise NotImplementedError
+        probs = np.zeros((len(states), 2))
+        probs[:, 0] = 0.5
+        probs[:, 1] = 0.5
+
         return probs
     
     def sample_action(self, state):
@@ -175,5 +179,61 @@ class RandomBlackjackPolicy(object):
             An action (int).
         """
         # YOUR CODE HERE
-        raise NotImplementedError
+        # raise NotImplementedError
+        if state[0] >= 20:
+            action = 0 # stick
+
+        else:
+            action = 1 # hit
+        
         return action
+
+def mc_importance_sampling(env, behavior_policy, target_policy, num_episodes, discount_factor=1.0,
+                           sampling_function=sample_episode):
+    """
+    Monte Carlo prediction algorithm. Calculates the value function
+    for a given target policy using behavior policy and ordinary importance sampling.
+    
+    Args:
+        env: OpenAI gym environment.
+        behavior_policy: A policy used to collect the data.
+        target_policy: A policy which value function we want to estimate.
+        num_episodes: Number of episodes to sample.
+        discount_factor: Gamma discount factor.
+        sampling_function: Function that generates data from one episode.
+    
+    Returns:
+        A dictionary that maps from state -> value.
+        The state is a tuple and the value is a float.
+    """
+
+    # Keeps track of current V and count of returns for each state
+    # to calculate an update.
+    V = defaultdict(float)
+    returns_count = defaultdict(float)
+
+    for episode in tqdm(range(num_episodes)):
+        states, actions, rewards, dones = sampling_function(env, behavior_policy)
+
+        G = 0
+        W = 1
+
+        for t in range(len(states))[::-1]:
+
+            G = discount_factor * G + rewards[t]
+            state = states[t]
+
+            if state not in states[:t]:
+                returns_count[state] += W
+                V[state] += (W / returns_count[state]) * (G - V[state])
+
+            if actions[t] != target_policy.sample_action(state):
+                break
+
+            W *= 1 / behavior_policy.get_probs([state], [actions[t]])[0, actions[t]]
+    
+    # YOUR CODE HERE
+    # raise NotImplementedError
+
+
+    return V
