78e1269aecfd94861b4ff966c4cfe9b40cb796b6
ilsefeenstra <ilsef@me.com>
2023-09-20 16:50:40 +0200

    new sample_policy

--- lab2/mc_autograde.py
+++ lab2/mc_autograde.py
@@ -66,127 +66,6 @@ class SimpleBlackjackPolicy(object):
         # End Code
         return action
 
-def sample_episode(env, policy):
-    """
-    A sampling routine. Given environment and a policy samples one episode and returns states, actions, rewards
-    and dones from environment's step function and policy's sample_action function as lists.
-
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-
-    Returns:
-        Tuple of lists (states, actions, rewards, dones). All lists should have same length. 
-        Hint: Do not include the state after the termination in the list of states.
-    """
-    states = []
-    actions = []
-    rewards = []
-    dones = []
-    
-    # YOUR CODE HERE
-    
-    state = env.reset()
-    done = False
-    
-    while not done:
-            
-        states.append(state)
-        action = policy.sample_action(state)
-        actions.append(action)
-        state, reward, done, _ = env.step(action)
-        rewards.append(reward)
-        dones.append(done)
-
-    # End Code
-
-    return states, actions, rewards, dones
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns_count = defaultdict(float)
-    
-    # YOUR CODE HERE
-    
-    for i in tqdm(range(num_episodes)):
-
-        states, actions, rewards, dones = sampling_function(env, policy)
-
-        G = 0
-
-        for t in reversed(range(len(states))):
-
-            G = discount_factor * G + rewards[t]
-            state = states[t]
-
-            if state not in states[:t]:
-                returns_count[state] += 1
-                V[state] += (G - V[state]) / returns_count[state]
-    
-    # End Code
-    return V
-
-class RandomBlackjackPolicy(object):
-    """
-    A random BlackJack policy.
-    """
-    def get_probs(self, states, actions):
-        """
-        This method takes a list of states and a list of actions and returns a numpy array that contains 
-        a probability of perfoming action in given state for every corresponding state action pair. 
-
-        Args:
-            states: a list of states.
-            actions: a list of actions.
-
-        Returns:
-            Numpy array filled with probabilities (same length as states and actions)
-        """
-        # YOUR CODE HERE
-        # raise NotImplementedError
-        probs = []
-        for state, action in zip(states, actions):
-            probs.append(0.5)
-        return np.array(probs)
-    
-    def sample_action(self, state):
-        """
-        This method takes a state as input and returns an action sampled from this policy.  
-
-        Args:
-            state: current state
-
-        Returns:
-            An action (int).
-        """
-        # YOUR CODE HERE
-        # raise NotImplementedError
-        if state[0] >= 20:
-            action = 0 # stick
-
-        else:
-            action = 1 # hit
-        
-        return action
-
 def mc_importance_sampling(env, behavior_policy, target_policy, num_episodes, discount_factor=1.0,
                            sampling_function=sample_episode):
     """
@@ -205,39 +84,36 @@ def mc_importance_sampling(env, behavior_policy, target_policy, num_episodes, di
         A dictionary that maps from state -> value.
         The state is a tuple and the value is a float.
     """
-
+    # Keeps track of current V and count of returns for each state
+    # to calculate an update.   
     V = defaultdict(float)
     returns_count = defaultdict(float)
-    returns_sum = defaultdict(float)
-    
-    # YOUR CODE HERE
-    
-    for i in tqdm(range(num_episodes)):
 
-        states, actions, rewards, dones = sampling_function(env, behavior_policy)
-
+    for episode in tqdm(range(num_episodes)): 
+        
+        states, actions, rewards, _ = sampling_function(env, behavior_policy)
+        
         G = 0
-        W = 1  # Importance sampling ratio, start with 1 as we'll multiply ratios for each step
+        W = 1
 
+        
         for t in reversed(range(len(states))):
-            state = states[t]
-            action = actions[t]
-            G = discount_factor * G + rewards[t]
+            state, action, reward = states[t], actions[t], rewards[t]
+            
+            # Update total return
+            G = discount_factor * G + reward
+            returns_count[state] += 1
 
-            # Calculate the importance sampling ratio for this step
-            behavior_prob = behavior_policy.get_probs([state], [action])[0]
-            target_prob = target_policy.get_probs([state], [action])[0]
-            W *= target_prob / behavior_prob
+            # Here, updating W before V as per your hint
+            W *= target_policy[state][action] / (behavior_policy[state][action])
 
-            # Update the cumulative returns and count for the state
-            if state not in states[:t]:  
-                returns_count[state] += 1
-                returns_sum[state] += W * G
-                V[state] = returns_sum[state] / returns_count[state]
-                
-            # If weight becomes 0, then further steps in the episode won't contribute
-            if W == 0:
+            # If behavior policy is zero for taken action, we can't use this data for off-policy learning
+            if behavior_policy[state][action] == 0:
                 break
-    # End Code
-    return V
+            
+            # Value update step using the formula: Vn = Vn−1 + (1/n) * (Wn * Gn − Vn−1)
+            V[state] += (W * G - V)
 
+            
+    return V
+        
