dd27b038af41b888161e6aca6563068f3dc579c4
Sergi095 <sergio.gutierrezmaury@gmail.com>
2023-09-24 11:44:17 +0200

    DQN-lab, need to submit

---
+++ lab4/dqn_autograde.py
@@ -0,0 +1,191 @@
+import numpy as np
+import random
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from tqdm import tqdm as _tqdm
+
+def tqdm(*args, **kwargs):
+    return _tqdm(*args, **kwargs, mininterval=1)  # Safety, do not overflow buffer
+
+class QNetwork(nn.Module):
+    
+    def __init__(self, num_hidden=128):
+        nn.Module.__init__(self)
+        self.l1 = nn.Linear(4, num_hidden)
+        self.l2 = nn.Linear(num_hidden, 2)
+
+    def forward(self, x):
+        # YOUR CODE HERE
+        # raise NotImplementedError
+        x = self.l1(x)
+        x = torch.relu(x)
+        x = self.l2(x)
+        return x
+
+class ReplayMemory:
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, transition):
+        # YOUR CODE HERE
+        # raise NotImplementedError
+        self.memory.append(transition)
+        if len(self.memory) > self.capacity:
+            self.memory.pop(0)
+
+    def sample(self, batch_size):
+        # YOUR CODE HERE
+        # raise NotImplementedError
+        return random.sample(self.memory, batch_size)
+
+
+    def __len__(self):
+        return len(self.memory)
+
+def get_epsilon(it):
+    # YOUR CODE HERE
+    # raise NotImplementedError
+    # it means iteration
+    if it < 1000:
+        epsilon = 1 - 0.9 * it / 1000
+    else:
+        epsilon = 0.05
+    return epsilon
+
+class EpsilonGreedyPolicy(object):
+    """
+    A simple epsilon greedy policy.
+    """
+    def __init__(self, Q, epsilon):
+        self.Q = Q
+        self.epsilon = epsilon
+    
+    def sample_action(self, obs):
+        """
+        This method takes a state as input and returns an action sampled from this policy.  
+
+        Args:
+            obs: current state
+
+        Returns:
+            An action (int).
+        """
+        # YOUR CODE HERE
+        # raise NotImplementedError
+        with torch.no_grad():
+            actions = self.Q(torch.tensor(obs).float())
+            max_index = torch.argmax(actions).item()
+            probabilities = [self.epsilon/2 + (1-self.epsilon) if i == max_index else self.epsilon/2 for i in range(2)]
+            action = np.random.choice([0, 1], p=probabilities)
+        return action
+
+        
+    def set_epsilon(self, epsilon):
+        self.epsilon = epsilon
+
+def compute_q_vals(Q, states, actions):
+    """
+    This method returns Q values for given state action pairs.
+    
+    Args:
+        Q: Q-net
+        states: a tensor of states. Shape: batch_size x obs_dim
+        actions: a tensor of actions. Shape: Shape: batch_size x 1
+
+    Returns:
+        A torch tensor filled with Q values. Shape: batch_size x 1.
+    """
+    # YOUR CODE HERE
+    # raise NotImplementedError
+    action = torch.zeros((actions.shape[0], 2))
+    action[range(actions.shape[0]), actions.squeeze()] = 1
+    return torch.sum(Q(states) * action, dim=1).unsqueeze(1)
+    
+    
+def compute_targets(Q, rewards, next_states, dones, discount_factor):
+    """
+    This method returns targets (values towards which Q-values should move).
+    
+    Args:
+        Q: Q-net
+        rewards: a tensor of rewards. Shape: Shape: batch_size x 1
+        next_states: a tensor of states. Shape: batch_size x obs_dim
+        dones: a tensor of boolean done flags (indicates if next_state is terminal) Shape: batch_size x 1
+        discount_factor: discount
+    Returns:
+        A torch tensor filled with target values. Shape: batch_size x 1.
+    """
+    # YOUR CODE HERE
+    # raise NotImplementedError
+    return rewards + discount_factor * torch.max(Q(next_states), dim=1)[0].unsqueeze(1) * (1 - dones)
+
+def train(Q, memory, optimizer, batch_size, discount_factor):
+    # DO NOT MODIFY THIS FUNCTION
+    
+    # don't learn without some decent experience
+    if len(memory) < batch_size:
+        return None
+
+    # random transition batch is taken from experience replay memory
+    transitions = memory.sample(batch_size)
+    
+    # transition is a list of 4-tuples, instead we want 4 vectors (as torch.Tensor's)
+    state, action, reward, next_state, done = zip(*transitions)
+    
+    # convert to PyTorch and define types
+    state = torch.tensor(state, dtype=torch.float)
+    action = torch.tensor(action, dtype=torch.int64)[:, None]  # Need 64 bit to use them as index
+    next_state = torch.tensor(next_state, dtype=torch.float)
+    reward = torch.tensor(reward, dtype=torch.float)[:, None]
+    done = torch.tensor(done, dtype=torch.uint8)[:, None]  # Boolean
+    
+    # compute the q value
+    q_val = compute_q_vals(Q, state, action)
+    with torch.no_grad():  # Don't compute gradient info for the target (semi-gradient)
+        target = compute_targets(Q, reward, next_state, done, discount_factor)
+    
+    # loss is measured from error between current and newly expected Q values
+    loss = F.smooth_l1_loss(q_val, target)
+
+    # backpropagation of loss to Neural Network (PyTorch magic)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    return loss.item()  # Returns a Python scalar, and releases history (similar to .detach())
+
+def run_episodes(train, Q, policy, memory, env, num_episodes, batch_size, discount_factor, learn_rate):
+    
+    optimizer = optim.Adam(Q.parameters(), learn_rate)
+    
+    global_steps = 0  # Count the steps (do not reset at episode start, to compute epsilon)
+    episode_durations = []  #
+    for i in range(num_episodes):
+        state = env.reset()
+        
+        steps = 0
+        while True:
+            # YOUR CODE HERE
+            # raise NotImplementedError
+            epsilon = get_epsilon(global_steps)
+            policy.set_epsilon(epsilon)
+            action = policy.sample_action(state)
+            next_state, reward, done, _ = env.step(action)
+            memory.push((state, action, reward, next_state, done))
+            train(Q, memory, optimizer, batch_size, discount_factor)
+            state = next_state
+            steps += 1
+            global_steps += 1
+            
+            if done:
+                if i % 10 == 0:
+                    print("{2} Episode {0} finished after {1} steps"
+                          .format(i, steps, '\033[92m' if steps >= 195 else '\033[99m'))
+                episode_durations.append(steps)
+                #plot_durations()
+                break
+    return episode_durations
